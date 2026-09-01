c706f8e4eb2d3251225edf4002c8ff3b5c8c18b9
Seop2 <lk9050@naver.com>
2026-09-01 13:50:51 +0900

    feat: implement mobile navigation menu with overlay toggle functionality

--- src/components/button/MobileButton.js
+++ src/components/button/MobileButton.js
@@ -1,10 +1,27 @@
+"use client"
 import Image from "next/image";
 import styles from "./MobileButton.module.css"
+import { useState } from "react";
+import Link from "next/link";
 
 export default function MobileButton() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen((prev) => !prev);
+    }
+
     return (
-        <button className={styles.btn}>
-            <Image src="/menu.png" alt="mobile" width={30} height={30} />
-        </button>
+        <>
+            {!isOpen && (<button className={styles.btn} onClick={toggleMenu}>
+                <Image src="/menu.png" alt="mobile" width={30} height={30} />
+            </button>)}
+            <div className={`${styles.overlay} ${isOpen ? styles.open : ""}`} onClick={() => setIsOpen(false)} />
+            <nav className={`${styles.mobileMenu} ${isOpen ? styles.open : ""}`}>
+                <Link href="/">Home</Link>
+                <Link href="/play">Play</Link>
+                <Link href="/rank">Ranking</Link>
+            </nav>
+        </>
     )
 }
