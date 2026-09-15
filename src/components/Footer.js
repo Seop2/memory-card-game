299dee445404d97ef101ea60a62d806d77a265bc
Seop2 <lk9050@naver.com>
2026-09-15 17:16:01 +0900

    feat: add GitHub repository link to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,9 @@
 import styles from "./Footer.module.css"
 export default function Footer() {
     return <footer className={styles.footer}>
-        <h2>Github Repository</h2>
+        <a href="https://github.com/Seop2/memory-card-game" target="_blank" rel="noopener noreferrer">
+            <h2>Github Repository</h2>
+        </a>
         <p>@2026 Memory Card Game All rights reserved</p>
     </footer>
 }
